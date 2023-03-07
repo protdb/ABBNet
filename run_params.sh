6b0448a3236ef3f6b90f9936356094229c2e51cc
prod <kirill@ibmc.org>
2023-03-07 19:46:52 +0300

    run_params.sh fixes from prod

--- run_params.sh
+++ run_params.sh
@@ -6,10 +6,12 @@ IMAGE_BASE_NAME="abbnet_search"
 declare -A DOCKER_PARAMS
 # launch parameters for every environment
 DOCKER_PARAMS["localhost"]=""
+DOCKER_PARAMS["test"]="--cpus=16 --shm-size=64G"
 
 declare -A VOLUMES
 # volume setup for every environment
 VOLUMES["localhost"]="-v /home/gluck/abb_data:/data -v /home/gluck/pdbs:/pdb"
+VOLUMES["test"]="-v /Data/abb_search_ws:/data -v /Data/PDB:/pdb"
 
 declare -A COMMANDS
 # available commands list
