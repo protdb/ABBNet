f79f3668e22f27e190f4006ab3c2467b1462266c
gluck <glucksistemi@gmail.com>
2023-03-17 12:52:23 +0300

    - Улучшения документации в run.sh

--- run.sh
+++ run.sh
@@ -25,22 +25,24 @@ do
     g) DOCKER_COMMAND="nvidia-docker run --gpus='\"device=${OPTARG}\"'";;
     h) echo "Common service runner script, configured to run container named $CONTAINER_BASE_NAME from image $IMAGE_BASE_NAME;
       options:
-      -e: environment identifier
+      MOST COMMON:
+      -e <environment>: environment identifier. Environment uses env/{environment}.env file as --env-file and adds volumes and options described for this environment in run_params.sh. Environment name will be added to container and image names
+      -d: detached mode. Container will run as detached and will not be removed after stop (default will add --rm to docker run; specifying -f flag here will replace it with -d --restart=always)
       -g <gpu_id>: If set, uses nvidia-docker instead of docker to run the worker with given GPU ID
-      -f <config file>: override environment file, default is 'env/{environment}.env'
-      -s <standalone_message_file>: if set, env var STANDALONE_MESSAGE will be passed to container overriding .env file
       -k: keep existing image. If set, script will not rebuild container
-      -i: interactive mode
+      SPECIFIC CASES:
       -c <command>: specifies the command to run in container. Commands must be listed in run_params.sh as elem of array COMMANDS. Default is no command provided (container will run wih CMD option from Dockerfile)
-      -d: container will run as detached and will not be removed after stop (default will add --rm to docker run, if specified there will be -d option added
+      -f <config file>: override environment file, default is 'env/{environment}.env'
+      -i: interactive mode (for commands supporting user interactions, like running bash in the container)
       -n: overwrite default container name configured as ${CONTAINER_BASE_NAME}_{ENVIRONMENT} to avoid conflicts with working container
-      -v <directory>: overrides volume mounting and mounts given URL to /data
+      -v <directory>: overrides ALL volume mounting configured in run_params.sh and mounts given path to /data
       -p: pure build (adds --no-cache to docker build command)
       -h: shows this help and exit.
       Common usages:
       run in prod: ./run.sh -de prod
-      enter interactive mode: ./run.sh -ikc bash
-      run with environment run_params using separate env-file:
+      run in prod using GPU with id 0: ./run.sh -de prod -g 0
+      enter interactive mode: ./run.sh -ikc bash (if bash is specified as command in run_params.sh
+      run using separate env-file:
           ./run.sh -e standalone -f env/override_params.env
       run using GPU:
          ./run.sh -e test -g 1
